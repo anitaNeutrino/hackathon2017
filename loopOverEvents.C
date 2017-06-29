f0d2feb7d4d513568270f70702dc054b8e44d46b
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-06-29 11:46:04 -0700

    commit simplest macro

---
+++ loopOverEvents.C
@@ -0,0 +1,30 @@
+/* simple example of how to loop over events with AnitaDataset 
+ *
+ * To run this, Set ANITA_ROOT_DATA to the mata downloaded by the script in this directory. 
+ *
+ * Otherwise you'll get an ugly crash
+ *
+ **/ 
+ 
+
+
+void loopOverEvents(int run=2017) 
+{
+
+
+
+  //Initialize the dataset
+  AnitaDataset d(run); 
+
+  //loop over AnitaDataset
+  for (int i = 0; i < d.N(); i++) 
+  {
+    //get the ith entry
+    d.getEntry(i); 
+
+    std::cout << "---------------------------------------------------------" << std::endl; 
+    std::cout << "I am the event with index " <<i << std::endl; 
+    std::cout << "My timestamp is " <<  d.header()->triggerTime << std::endl; 
+  }
+} 
+
