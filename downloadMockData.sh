a8c75043fed53ef4b21a3539489718c81e65c88d
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-06-29 11:31:06 -0700

    don't overwrite the same file over and over again :)

--- downloadMockData.sh
+++ downloadMockData.sh
@@ -10,9 +10,9 @@ url=$url_uchicago
 
 mkdir -p data/run$run
 curl $url/SimulatedAnitaEventFile.root > data/run$run/SimulatedAnitaEventFile$run.root
-curl $url/SimulatedAnitaHeadFile.root > data/run$run/SimulatedAnitaEventFile$run.root
-curl $url/SimulatedAnitaTruthFile.root > data/run$run/SimulatedAnitaEventFile$run.root
-curl $url/SimulatedAnitaGpsFile.root > data/run$run/SimulatedAnitaEventFile$run.root
+curl $url/SimulatedAnitaHeadFile.root > data/run$run/SimulatedAnitaHeadtFile$run.root
+curl $url/SimulatedAnitaTruthFile.root > data/run$run/SimulatedAnitaTruthFile$run.root
+curl $url/SimulatedAnitaGpsFile.root > data/run$run/SimulatedAnitaGpsFile$run.root
 
 
 
