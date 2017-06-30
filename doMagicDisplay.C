533b8a911900b6d755db8e18d8f9203d577f2700
Ben Rotter <BenJRotter@gmail.com>
2017-06-30 11:56:07 -0700

    added a Hello file and changed doMagicDisplay.C to do ANITA3

--- doMagicDisplay.C
+++ doMagicDisplay.C
@@ -2,9 +2,13 @@
  * Macro to run Magic Display (very simple!)
  *
  */
+#include "AnitaVersion.h"
+
 
 void doMagicDisplay(int run=2017)
 {
+  AnitaVersion::set(3);
+
   MagicDisplay * magic = new MagicDisplay(run); 
   magic->startEventDisplay(); 
 }
